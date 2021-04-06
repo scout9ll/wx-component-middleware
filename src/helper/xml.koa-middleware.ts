1fb67d1caf9d7171a7ce422348e60efae8706d45
steve9II <36192834+steve9II@users.noreply.github.com>
2021-04-06 14:53:47 +0800

    fix:avoid middleware conflict between xml and text

--- src/helper/xml.koa-middleware.ts
+++ src/helper/xml.koa-middleware.ts
@@ -1,6 +1,8 @@
 
 export default async function xmLMiddleWare(ctx, next) {
   if (ctx.request.header['content-type']?.includes('xml')) {
+    // avoid conflict between xml and text
+    ctx.request.header['content-type'] = 'application/xml'
     const xml: string = await new Promise((resolve) => {
       let data = '';
       ctx.req.on('data', (chunk) => data += chunk);
